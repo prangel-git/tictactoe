e5dd2297c862141a5fb10a3104a78379e4e3ea1c
prangel-git <62568827+prangel-git@users.noreply.github.com>
2021-04-04 17:05:42 -0400

    Elminating display implementation for agentid.

--- src/agentid.rs
+++ src/agentid.rs
@@ -1,16 +1,7 @@
-use std::fmt::{Display, Formatter, Result};
+// use std::fmt::{Display, Formatter, Result};
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
 pub enum AgentId {
     X,
     O,
-}
-
-impl Display for AgentId {
-    fn fmt(&self, f: &mut Formatter) -> Result {
-        match self {
-            AgentId::X => write!(f, "Player X"),
-            AgentId::O => write!(f, "Player O"),
-        }
-    }
 }
